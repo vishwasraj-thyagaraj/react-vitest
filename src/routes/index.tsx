fdb9681d08629c99fc134f24396597505decef59
Vishwasraj <vishwasraj.thyagaraj@freshworks.com>
2024-09-18 13:34:43 +0530

    added scss support

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
+import Button from '@/components/button';
 
 export const Route = createFileRoute('/')({
-  component: () => <div>hello world</div>,
+  component: () => <div><Button/></div>,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,24 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import autoprefixer from 'autoprefixer';
+import * as sass from 'sass';
 
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  css: {
+    devSourcemap: true,
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+    postcss: {
+      plugins: [
+        autoprefixer({}), // add options if needed
+      ],
+    },
+  },
   test: {
     globals: true,
   },
