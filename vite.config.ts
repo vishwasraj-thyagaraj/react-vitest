d96b5e8f5050a619eb9576dcf7073ef0400e1634
Vishwasraj <vishwasraj.thyagaraj@freshworks.com>
2024-09-18 16:25:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,8 +22,6 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./vitest.setup.ts",
     include: ["tests/*.test.tsx"],
     browser: {
       provider: "preview",
