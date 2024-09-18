0abd47ec3e4d56c8274c9db0f07054fb4b7a18ee
Vishwasraj <vishwasraj.thyagaraj@freshworks.com>
2024-09-18 12:40:28 +0530

    Init

---
+++ src/routes/__root.tsx
@@ -0,0 +1,12 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
