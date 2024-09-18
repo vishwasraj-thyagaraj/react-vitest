53d3a356eb6aaa2e3f06b14f66d2d13cc8b9b6eb
Vishwasraj <vishwasraj.thyagaraj@freshworks.com>
2024-09-18 12:49:52 +0530

    add coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  test: {
+    globals: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
+  }
 });
